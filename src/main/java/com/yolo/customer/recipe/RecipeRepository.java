9611964ee000e6dad6a1b6066d300b512d724925
AHMAD <89203329+Ahmad-981@users.noreply.github.com>
2024-08-23 10:42:36 +0500

    Add PMD, SpotBugs, and Jacoco plugins for code quality checks

--- src/main/java/com/yolo/customer/recipe/RecipeRepository.java
+++ src/main/java/com/yolo/customer/recipe/RecipeRepository.java
@@ -1,7 +1,7 @@
 package com.yolo.customer.recipe;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
 //@Repository
 //public interface recipeRepository extends JpaRepository<Recipe, Integer> {
 //
