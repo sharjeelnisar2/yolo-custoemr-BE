ea2af6a9d4f826d00ba3cd623d7dc0893a734e80
eshah.hamid <eshahhamid02@gmail.com>
2024-08-28 17:33:19 +0500

    create user profile

---
+++ src/main/java/com/yolo/customer/userProfile/UserProfileController.java
@@ -0,0 +1,24 @@
+package com.yolo.customer.userProfile;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserProfileController {
+    @Autowired
+    private UserProfileService userProfileService;
+
+    @PreAuthorize("hasRole('ROLE_CREATE_PROFILE')")
+    @PostMapping("/{username}/profiles")
+    public ResponseEntity<String> createUserProfile(
+            @PathVariable String username,
+            @Valid @RequestBody UserProfileRequestDTO userProfileRequest) {
+        System.out.println(userProfileRequest.getHouse());
+        UserProfile userProfile = userProfileService.createUserProfile(username, userProfileRequest);
+        return ResponseEntity.ok("User profile created successfully.");
+    }
+}
