fac762bef27a65192c036185cdeb54ca943a6713
eshah.hamid <eshahhamid02@gmail.com>
2024-08-29 11:36:02 +0500

    code uncommented

--- src/main/java/com/yolo/customer/config/SecurityConfig.java
+++ src/main/java/com/yolo/customer/config/SecurityConfig.java
@@ -16,16 +16,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SecurityConfig {
 
     private final JwtConfig jwtConfig;
-    //private final JWTAuthenticationFilter jwtAuthenticationFilter;
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
 
-//    public SecurityConfig(JwtConfig jwtConfig, JWTAuthenticationFilter jwtAuthenticationFilter) {
-//        this.jwtConfig = jwtConfig;
-//        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-//    }
-
-    public SecurityConfig(JwtConfig jwtConfig) {
+    public SecurityConfig(JwtConfig jwtConfig, JWTAuthenticationFilter jwtAuthenticationFilter) {
         this.jwtConfig = jwtConfig;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -34,7 +31,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated()
                 )
-               // .addFilterBefore(jwtAuthenticationFilter, org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthenticationFilter, org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationFilter.class)
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConfig.jwtAuthenticationConverter()))
                 )
